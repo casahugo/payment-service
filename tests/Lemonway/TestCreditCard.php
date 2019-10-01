826923d4dc1fce80fd8456b60f13f9b69fcea87f
Hugo Casabella <hugo@wizacha.com>
2019-10-01 16:18:28 +0200

    configure phpunit

---
+++ tests/Lemonway/TestCreditCard.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author      Wizacha DevTeam <dev@wizacha.com>
+ * @copyright   Copyright (c) Wizacha
+ * @license     Proprietary
+ */
+declare(strict_types=1);
+
+namespace App\Tests\Lemonway;
+
+use PHPUnit\Framework\TestCase;
+
+class TestCreditCard extends TestCase
+{
+    public function testCreditCard(): void
+    {
+        static::assertTrue(true);
+    }
+}
